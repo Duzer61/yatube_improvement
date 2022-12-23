8aadeacf24efeecc767b32538897fe643b011ffa
Данил Кочетов <dmi194@yandex.ru>
2022-12-23 16:10:19 +0300

    Добавлена сырая функция удаления поста

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -97,6 +97,16 @@ def post_edit(request, post_id):
 
 
 @login_required
+def post_delete(request, post_id):
+    """Удаляет пост если удаляет сам автор"""
+    post = get_object_or_404(Post, pk=post_id)
+    if post.author != request.user:
+        return redirect('posts:post_detail', post_id)
+    post.delete()
+    return redirect('posts:profile', request.user)
+
+
+@login_required
 def add_comment(request, post_id):
     """Добавляет комментарий к посту"""
     post = get_object_or_404(Post, pk=post_id)
